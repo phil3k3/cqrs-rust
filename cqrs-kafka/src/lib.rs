c32885513491ac932e6431ac9ee60dd9041f5999
philipx333 <philip.limbeck@gmail.com>
2023-03-06 20:39:46 +0100

    Fix Kafka tests

--- cqrs-kafka/src/lib.rs
+++ cqrs-kafka/src/lib.rs
@@ -1,15 +1,42 @@
-use std::fmt::format;
-use rdkafka::{ClientConfig, ClientContext, Message};
+use std::time::Duration;
+use rdkafka::{ClientConfig, ClientContext, Message, TopicPartitionList};
 use rdkafka::config::RDKafkaLogLevel;
-use rdkafka::error::KafkaError;
+use rdkafka::consumer::{BaseConsumer, Consumer, ConsumerContext, Rebalance};
+use rdkafka::error::{KafkaError, KafkaResult};
 use rdkafka::message::DeliveryResult;
-use rdkafka::producer::{BaseProducer, BaseRecord, ProducerContext, ThreadedProducer};
+use rdkafka::producer::{BaseRecord, ProducerContext, ThreadedProducer};
 use cqrs_library::{InboundChannel, OutboundChannel};
+use log::{info};
+
 
 struct ProducerCallbackLogger;
 impl ClientContext for ProducerCallbackLogger {
 
 }
+
+// A context can be used to change the behavior of producers and consumers by adding callbacks
+// that will be executed by librdkafka.
+// This particular context sets up custom callbacks to log rebalancing events.
+struct CustomContext;
+
+impl ClientContext for CustomContext {}
+
+impl ConsumerContext for CustomContext {
+    fn pre_rebalance(&self, rebalance: &Rebalance) {
+        info!("Pre rebalance {:?}", rebalance);
+    }
+
+    fn post_rebalance(&self, rebalance: &Rebalance) {
+        info!("Post rebalance {:?}", rebalance);
+    }
+
+    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
+        info!("Committing offsets: {:?}", result);
+    }
+}
+
+// A type alias with your custom consumer can be created for convenience.
+type LoggingConsumer = BaseConsumer<CustomContext>;
 
 pub struct KafkaOutboundChannel {
     topic: String,
@@ -17,9 +44,10 @@ pub struct KafkaOutboundChannel {
 }
 
 pub struct KafkaInboundChannel {
-    topic: String,
-    bootstrap_server: String,
+    consumer: BaseConsumer<CustomContext>
 }
+
+
 
 impl ProducerContext for ProducerCallbackLogger {
     type DeliveryOpaque = ();
@@ -43,11 +71,7 @@ fn create_producer(bootstrap_server: String, service_id: &str) -> Result<Threade
     let mut config = ClientConfig::new();
     config
         .set("bootstrap.servers", bootstrap_server)
-        .set("message.timeout.ms", "5000")
-        .set("enable.idempotence", "true")
-        .set("transactional.id", format!("{}-producer", &service_id))
-        .set("debug", "eos")
-        .set("client.id", format!("{}-producer", &service_id));
+        .set("message.timeout.ms", "5000");
     config.set_log_level(RDKafkaLogLevel::Debug);
     config.create_with_context(ProducerCallbackLogger {})
 }
@@ -72,8 +96,66 @@ impl OutboundChannel for KafkaOutboundChannel {
     }
 }
 
+impl KafkaInboundChannel {
+    pub fn new(service_id: &str,topics: &[&str], bootstrap_server: &str) -> KafkaInboundChannel {
+       let channel = KafkaInboundChannel {
+            consumer: create_consumer(bootstrap_server.to_string(), service_id.to_string()).unwrap()
+        };
+        channel.consumer.subscribe(&topics.to_vec()).expect("Could not subscribe");
+        channel
+    }
+}
+
+fn create_consumer(bootstrap_server: String, service_id: String) -> Result<LoggingConsumer, KafkaError> {
+    let mut config = ClientConfig::new();
+    config
+        .set("group.id", format!("{}-consumer", &service_id))
+        .set("bootstrap.servers", &bootstrap_server)
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "false")
+        .set("auto.offset.reset", "earliest");
+
+   // all nodes of the same service are in a group and will get some partitions assigned
+    config.set_log_level(RDKafkaLogLevel::Debug);
+    config.create_with_context(CustomContext {})
+}
+
 impl InboundChannel for KafkaInboundChannel {
     fn consume(&mut self) -> Option<Vec<u8>> {
-        todo!()
+        let message = self.consumer.poll(Duration::from_secs(30));
+        return message.map(|x| x.expect("Could not read message").payload().unwrap().to_vec())
+    }
+}
+
+struct Runtime {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use testcontainers::{clients, images::kafka};
+    use cqrs_library::{InboundChannel, OutboundChannel};
+    use crate::{KafkaInboundChannel, KafkaOutboundChannel};
+
+    #[test]
+    fn test_sync_send_receive() {
+
+        let docker = clients::Cli::default();
+        let kafka_node = docker.run(kafka::Kafka::default());
+
+        let bootstrap_servers = format!(
+            "127.0.0.1:{}",
+            kafka_node.get_host_port_ipv4(kafka::KAFKA_PORT)
+        );
+
+        let mut outbound_channel = KafkaOutboundChannel::new("TEST_OUT", "TEST", &bootstrap_servers);
+
+        outbound_channel.send("KEY".as_bytes().to_vec(), "MESSAGE".as_bytes().to_vec());
+
+        let mut inbound_channel = KafkaInboundChannel::new("TEST_IN", &["TEST"], &bootstrap_servers);
+
+        let message = inbound_channel.consume().expect("Message not received");
+
+        assert_eq!("MESSAGE", String::from_utf8(message).unwrap());
     }
 }
